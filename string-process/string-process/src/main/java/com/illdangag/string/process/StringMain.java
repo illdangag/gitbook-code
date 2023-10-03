2a127d0c0412a7295c1835714d08a8b0e4898646
ybchoi <ybchoi@ybchoi-macbook-16.local>
2023-10-03 17:33:25 +0900

    string process

---
+++ string-process/string-process/src/main/java/com/illdangag/string/process/StringMain.java
@@ -0,0 +1,11 @@
+package com.illdangag.string.process;
+
+public class StringMain {
+    public static void main(String[] args) {
+        String text00 = "TEXT00";
+        String text01 = "TEXT01";
+        String text02 = "TEXT02";
+        String text03 = text00 + text01 + text02 + "TEST03";
+        String text04 = "TEST" + "0" + "4";
+    }
+}
